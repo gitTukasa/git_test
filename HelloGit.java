102c0706e276c3c227ef0dbc0778f580069998f7
gitTukasa <12752043infin@gmail.com>
2020-02-22 18:10:36 +0900

    Eclipse Test

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,11 @@
+import java.util.stream.IntStream;
+
 class HelloGit{
 
 	public static void main(String... args){
 		System.out.println("Hello Git!!");
-		System.out.println("Hello Git!!");
+		IntStream.range(0, 10){
+			System.out.println(i);
+		}
 	}
 }
